85b4ea34aaa8693dd45c726637a72942ec1a35b2
Gustavo Silva <gustavo_almeida11@hotmail.com>
2022-09-21 17:44:57 -0300

    consulta por datas

--- backend/src/main/java/com/gusta/devsuperior/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/gusta/devsuperior/dsmeta/controllers/SaleController.java
@@ -2,12 +2,14 @@ package com.gusta.devsuperior.dsmeta.controllers;
 
 import com.gusta.devsuperior.dsmeta.entities.Sale;
 import com.gusta.devsuperior.dsmeta.services.SaleService;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,8 +20,11 @@ public class SaleController {
     private final SaleService service;
 
     @GetMapping
-    public ResponseEntity<List<Sale>> findSales() {
-        var allSales = service.findSales();
+    public ResponseEntity<Page<Sale>> findSales(@RequestParam(value = "minDate", defaultValue = "") String minDate,
+                                                @RequestParam(value = "maxDate", defaultValue = "") String maxDate,
+                                                Pageable pageable) {
+
+        var allSales = service.findSales(minDate, maxDate,pageable);
 
         if (allSales.isEmpty()) {
             return ResponseEntity.notFound().build();
--- backend/src/main/java/com/gusta/devsuperior/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/gusta/devsuperior/dsmeta/services/SaleService.java
@@ -2,9 +2,13 @@ package com.gusta.devsuperior.dsmeta.services;
 
 import com.gusta.devsuperior.dsmeta.entities.Sale;
 import com.gusta.devsuperior.dsmeta.repositories.SaleRepository;
-import java.util.List;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,8 +17,13 @@ public class SaleService {
 
     private final SaleRepository saleRepository;
 
-    public List<Sale> findSales() {
-        return saleRepository.findAll();
+    public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
+        var today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+
+        var min = minDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate);
+        var max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
+
+        return saleRepository.findSalesBetween(min, max, pageable);
     }
 
 }
